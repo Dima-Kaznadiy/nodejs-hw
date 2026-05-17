12228f52711fa57aa27d7d680082ad68b5f4f60a
Dima Kaznadiy <dimadanced@gmail.com>
2026-05-17 23:05:32 +0300

    mod-2

--- src/controllers/notesController.js
+++ src/controllers/notesController.js
@@ -34,3 +34,47 @@ export const getAllNotes = async (req, res) => {
     notes,
   });
 };
+
+export const getNoteById = async (req, res) => {
+  const { noteId } = req.params;
+
+  const note = await Note.findById(noteId);
+
+  if (!note) {
+    throw createHttpError(404, 'Note not found');
+  }
+
+  res.status(200).json(note);
+};
+
+export const createNote = async (req, res) => {
+  const note = await Note.create(req.body);
+
+  res.status(201).json(note);
+};
+
+export const deleteNote = async (req, res) => {
+  const { noteId } = req.params;
+
+  const note = await Note.findByIdAndDelete(noteId);
+
+  if (!note) {
+    throw createHttpError(404, 'Note not found');
+  }
+
+  res.status(200).json(note);
+};
+
+export const updateNote = async (req, res) => {
+  const { noteId } = req.params;
+
+  const note = await Note.findByIdAndUpdate(noteId, req.body, {
+    returnDocument: 'after',
+  });
+
+  if (!note) {
+    throw createHttpError(404, 'Note not found');
+  }
+
+  res.status(200).json(note);
+};
