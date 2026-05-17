f0b9952a632cea4ec6e8cc4354c7ecc035155e77
Dima Kaznadiy <dimadanced@gmail.com>
2026-05-17 23:13:09 +0300

    mod-4

--- src/controllers/notesController.js
+++ src/controllers/notesController.js
@@ -1,3 +1,6 @@
+import createHttpError from 'http-errors';
+import { Note } from '../models/note.js';
+
 export const getAllNotes = async (req, res) => {
   const { page = 1, perPage = 10, tag, search } = req.query;
 
@@ -42,4 +45,48 @@ export const getAllNotes = async (req, res) => {
     totalPages,
     notes,
   });
+};
+
+export const getNoteById = async (req, res) => {
+  const { noteId } = req.params;
+
+  const note = await Note.findById(noteId);
+
+  if (!note) {
+    throw createHttpError(404, 'Note not found');
+  }
+
+  res.status(200).json(note);
+};
+
+export const createNote = async (req, res) => {
+  const note = await Note.create(req.body);
+
+  res.status(201).json(note);
+};
+
+export const deleteNote = async (req, res) => {
+  const { noteId } = req.params;
+
+  const note = await Note.findByIdAndDelete(noteId);
+
+  if (!note) {
+    throw createHttpError(404, 'Note not found');
+  }
+
+  res.status(200).json(note);
+};
+
+export const updateNote = async (req, res) => {
+  const { noteId } = req.params;
+
+  const note = await Note.findByIdAndUpdate(noteId, req.body, {
+    returnDocument: 'after',
+  });
+
+  if (!note) {
+    throw createHttpError(404, 'Note not found');
+  }
+
+  res.status(200).json(note);
 };
