5e7feff1bb65d334b161d1974db3885fb3753917
Dima Kaznadiy <dimadanced@gmail.com>
2026-05-25 23:23:12 +0300

    mod-3

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,4 +1,17 @@
-POST /auth/register
-POST /auth/login
-POST /auth/refresh
-POST /auth/logout
+import { Router } from 'express';
+
+import {
+  registerUser,
+  loginUser,
+  refreshUserSession,
+  logoutUser,
+} from '../controllers/authController.js';
+
+const router = Router();
+
+router.post('/auth/register', registerUser);
+router.post('/auth/login', loginUser);
+router.post('/auth/refresh', refreshUserSession);
+router.post('/auth/logout', logoutUser);
+
+export default router;
