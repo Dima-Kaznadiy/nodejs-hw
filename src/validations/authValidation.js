48bd7fe337b8de3066f7772704685946ec74f010
Dima Kaznadiy <dimadanced@gmail.com>
2026-05-25 23:39:52 +0300

    mod-7

--- src/validations/authValidation.js
+++ src/validations/authValidation.js
@@ -1,11 +1,15 @@
-import Joi from 'joi';
+import { Joi } from 'celebrate';
 
-export const registerUserSchema = Joi.object({
-  email: Joi.string().email().required(),
-  password: Joi.string().min(8).required(),
-});
+export const registerUserSchema = {
+  body: Joi.object({
+    email: Joi.string().email().required(),
+    password: Joi.string().min(8).required(),
+  }),
+};
 
-export const loginUserSchema = Joi.object({
-  email: Joi.string().email().required(),
-  password: Joi.string().required(),
-});
+export const loginUserSchema = {
+  body: Joi.object({
+    email: Joi.string().email().required(),
+    password: Joi.string().required(),
+  }),
+};
